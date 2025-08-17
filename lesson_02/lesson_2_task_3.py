53d47df65228beb8c006f3093696b8e24345ac22
Victory-sky111 <shavitok88@gmail.com>
2025-08-17 13:22:48 +0300

    lesson_02

---
+++ lesson_02/lesson_2_task_3.py
@@ -0,0 +1,11 @@
+import math
+
+
+def square(side):
+    return (math.ceil(sq))
+
+
+side = input("Введите длину стороны квадрата: ")
+side = float(side)
+sq = side*side
+print(f"Площадь квадрата равна: {square(sq)}")
